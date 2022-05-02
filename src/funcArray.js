7cdba866db933a30ec94fce23d5213405576a851
Your Name <nedeltoga@gmail.com>
2022-05-02 15:02:19 +0200

    test1 y dos pasados

--- src/funcArray.js
+++ src/funcArray.js
@@ -1,9 +1,51 @@
 /* Encuentra el máximo */
 
-export function maxOfTwoNumbers(){}
-export function findLongestWord(){}
+export function maxOfTwoNumbers(a, b){
+    if ( a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+// Encuentra la palabra más larga
+
+let words = [
+    'mystery',
+    'brother',
+    'aviator',
+    'crocodile',
+    'pearl',
+    'orchard',
+    'crackpot'
+  ];
+
+export function findLongestWord(words) {
+    if (words.length === 0) 
+    { 
+        return undefined
+    }
+    if (words.length === 1) {
+        return words[0]
+    }
+    let longest = "" 
+    for (let i=0; i < words.length; i++) {
+        if (words[i].length > longest.length)
+         longest = words[i]
+    }
+    
+    return longest
+}
+
+//Calcula la suma
+
 export function sumArray(){}
+
+// Calcula la media aritmética
+
 export function averageNumbers(){}
+
+
 export function averageWordLength(){}
 export function uniquifyArray(){}
 export function doesWordExist(){}
